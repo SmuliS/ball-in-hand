126e9d436843e7b427040bb5b9dcdc6b39c701e6
Samuli Suortti <samuli.suortti@gmail.com>
2013-06-19 20:08:00 +0300

    project skeleton added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BallInHand::Application.config.secret_token = '9fe319d79a505ac5d1ccbdd11a77a60373d151368ba3d1500b2df48bd94c6473b4d2ce74643ad1b13317034424794715b8fcc386470972a901a43dcccb2f6424'
