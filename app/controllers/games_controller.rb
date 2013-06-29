bfbdd263a968996ddc670e451e5cd47e3b28d9f8
Samuli Suortti <samuli.suortti@gmail.com>
2013-06-29 18:33:53 +0300

    Added feature to create new game records

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,19 @@
 class GamesController < ApplicationController
+  def index
+    @players = Player.all.sort_by { |player| - player.rating}
+  end
+
+  def new
+    @players = Player.all
+  end
 
   def create
-    winner_name = params[:winner].to_s
-    loser_name = params[:loser].to_s
+    winner = params[:winner].to_i
+    loser  = params[:loser].to_i
     game_type = params[:game_type].to_s
 
-    winner = Player.find_by_name(winner_name)
-    loser = Player.find_by_name(loser_name)
+    winner = Player.find(winner)
+    loser = Player.find(loser)
 
     Game.record game_type, winner, loser
     redirect_to "/" # TODO: RETURN 200 AND SOME FANCY JSON FOR BACKBONE
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,17 +1,29 @@
 require 'spec_helper'
 
 describe GamesController do
+  describe "#index" do
+    it "should return status code 200" do
+      get :index
+      response.status.should eq 200
+    end
+  end
+  describe "#new" do
+    it "should return status code 200" do
+      get :new
+      response.status.should eq 200
+    end
+  end
   describe "#create" do
     let(:p1) {create(:player)}
     let(:p2) {create(:player)}
     it "should create game" do
       expect{
-        post :create, winner: p1.name, loser: p2.name, game_type: "EightBall"
+        post :create, winner: p1.id, loser: p2.id, game_type: "EightBall"
       }.to change{Game.count}.by(1)
     end
 
     it "should redirect to main page" do
-      post :create, winner: p1.name, loser: p2.name, game_type: "EightBall"
+      post :create, winner: p1.id, loser: p2.id, game_type: "EightBall"
 
       response.should redirect_to("/")
     end
