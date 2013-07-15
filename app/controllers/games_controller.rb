a9d39544a8d2052fc1511e919e7451fb4fe5995c
Samuli Suortti <samuli.suortti@gmail.com>
2013-07-15 20:38:44 +0300

    Added validation to player secretkeys

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,13 +9,17 @@ class GamesController < ApplicationController
 
   def create
     winner = params[:winner].to_i
+    winner_secret_key = params[:winner_secret_key]
     loser  = params[:loser].to_i
+    loser_secret_key = params[:loser_secret_key]
     game_type = params[:game_type].to_s
 
     winner = Player.find(winner)
     loser = Player.find(loser)
 
-    if winner != loser
+    authenticated = winner.secret_key == winner_secret_key && loser.secret_key == loser_secret_key
+
+    if winner != loser && authenticated
       Game.record game_type, winner, loser
       redirect_to "/" # TODO: RETURN 200 AND SOME FANCY JSON FOR BACKBONE
     else
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -18,19 +18,32 @@ describe GamesController do
     let(:p2) {create(:player)}
     it "should create game" do
       expect{
-        post :create, winner: p1.id, loser: p2.id, game_type: "EightBall"
+        post :create, winner: p1.id, winner_secret_key: p1.secret_key, loser: p2.id, loser_secret_key: p2.secret_key, game_type: "EightBall"
       }.to change{Game.count}.by(1)
     end
 
     it "should redirect to main page" do
-      post :create, winner: p1.id, loser: p2.id, game_type: "EightBall"
+      post :create, winner: p1.id, winner_secret_key: p1.secret_key, loser: p2.id, loser_secret_key: p2.secret_key, game_type: "EightBall"
 
       response.should redirect_to("/")
     end
-
+    describe "it should check secret_keys" do
+      context "valid keys" do
+        it "should record a game" do
+          post :create, winner: p1.id, winner_secret_key: p1.secret_key, loser: p2.id, loser_secret_key: p2.secret_key, game_type: "EightBall"
+          response.should redirect_to("/")
+        end
+      end
+      context "invalid keys" do
+        it "should not record a game" do
+          expect{
+            post :create, winner: p1.id, winner_secret_key: "wrong_key", loser: p2.id, loser_secret_key: p2.secret_key, game_type: "EightBall"
+          }.to change{Game.count}.by(0)
+        end
+      end
+    end
     it "should render new if winner and loser are same" do
-      post :create, winner: p1.id, loser: p1.id, game_type: "EightBall"
-
+      post :create, winner: p1.id, winner_secret_key: p1.secret_key, loser: p1.id, loser_secret_key: p1.secret_key, game_type: "EightBall"
       response.should render_template :new
     end
   end
